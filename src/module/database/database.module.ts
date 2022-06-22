6ae0d7c3ba1d0bf0904c7e2f10ea5d5555956ad3
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2022-06-21 21:42:13 -0300

    style: fix style for the file

--- src/module/database/database.module.ts
+++ src/module/database/database.module.ts
@@ -5,21 +5,21 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: process.env.NODE_ENV === 'test' ? ['.env.test'] : ['.env'],
-        }),
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: process.env.TYPEORM_HOST,
-            port: Number(process.env.TYPEORM_PORT),
-            username: process.env.TYPEORM_USERNAME,
-            password: process.env.TYPEORM_PASSWORD,
-            database: process.env.TYPEORM_DATABASE,
-            entities: [Task, User],
-            synchronize: process.env.NODE_ENV === 'test',
-            logging: false,
-        }),
-    ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'test' ? ['.env.test'] : ['.env']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [Task, User],
+      synchronize: process.env.NODE_ENV === 'test',
+      logging: false
+    })
+  ]
 })
-export default class DatabaseModule { }
+export default class DatabaseModule {}
