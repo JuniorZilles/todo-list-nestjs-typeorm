032e1e8b097fc79b021027aded3c53cb290279da
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2022-06-26 16:06:59 -0300

    test: start of implementation of user create

--- test/user/user.service.spec.ts
+++ test/user/user.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '../src/service/user/user.service';
+import CreateUserDto from 'src/dto/user/create-user.dto';
+import { createOneUser } from 'test/utils/factory/user.factory';
+import UserService from '../../src/service/user/user.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('src :: service :: UserService()', () => {
+  describe('GIVEN a mocked repository with ZERO users', () => {
+    let service: UserService;
+    const userFactory = createOneUser();
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [UserService]
+      }).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+      service = module.get<UserService>(UserService);
+    });
 
-    service = module.get<UserService>(UserService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    describe('WHEN adding a valid user', () => {
+      let createdUser: CreateUserDto;
+      beforeEach(async () => {
+        createdUser = await service.create(userFactory);
+      });
+      it('THEN it should return the object with a id', () => {
+        expect(createdUser).toBeDefined();
+        expect(createdUser).toHaveProperty('id');
+        expect(createdUser.id).toBeDefined();
+      });
+    });
   });
 });
