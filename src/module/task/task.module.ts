7c15b279be2fb90db526d05d5f3bdd3cd65004a4
Junior Zilles <junior_zilles@hotmail.com>
2022-07-08 16:38:46 -0300

    fix: missing injection of repository

--- src/module/task/task.module.ts
+++ src/module/task/task.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import TaskService from '../../service/task/task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import Task from '../../entities/task/task.entity';
+import User from '../../entities/user/user.entity';
 import TaskController from '../../controller/task/task.controller';
+import TaskService from '../../service/task/task.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [TaskService]
+  providers: [TaskService],
+  imports: [TypeOrmModule.forFeature([Repository<Task>, Repository<User>])]
 })
 export default class TaskModule {}
--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import UserService from '../../service/user/user.service';
 import UserController from '../../controller/user/user.controller';
+import User from '../../entities/user/user.entity';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports: [TypeOrmModule.forFeature([Repository<User>])]
 })
 export default class UserModule {}
