9e4fbfd38223721e847d36be2d8f3e9af6dc652a
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2022-07-14 21:09:04 -0300

    test: add missing validations for creating a user

--- test/user/feature/user.create.e2e-spec.ts
+++ test/user/feature/user.create.e2e-spec.ts
@@ -2,9 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import AppModule from '../../../src/module/app.module';
-import { USER } from '../../utils/factory/user.factory';
+import { createOnePOSTUser } from '../../utils/factory/user.factory';
 
-describe('src :: user :: e2e', () => {
+describe('src :: user :: e2e :: POST', () => {
   describe('GIVEN a mocked repository with ZERO users', () => {
     let app: INestApplication;
 
@@ -30,9 +30,19 @@ describe('src :: user :: e2e', () => {
       await app.close();
     });
 
-    describe('WHEN creating a valid new user / (POST)', () => {
+    const fixedUser1 = createOnePOSTUser();
+    const fixedUser2 = createOnePOSTUser();
+    beforeEach(async () => {
+      await Promise.all([
+        request(app.getHttpServer()).post('/user').send(fixedUser1),
+        request(app.getHttpServer()).post('/user').send(fixedUser2)
+      ]);
+    });
+
+    describe('WHEN creating a valid new user', () => {
       it('THEN it should return status 201 with the body', async () => {
-        const response = await request(app.getHttpServer()).post('/user').send(USER);
+        const user = createOnePOSTUser();
+        const response = await request(app.getHttpServer()).post('/user').send(user);
         const { body, status } = response;
 
         expect(status).toBe(201);
@@ -52,15 +62,88 @@ describe('src :: user :: e2e', () => {
           zipCode: expect.any(String)
         });
 
-        expect(body.cpf).toBe(USER.cpf);
-        expect(body.birthday).toBe(USER.birthday);
-        expect(body.name).toBe(USER.name);
-        expect(body.email).toBe(USER.email);
-        expect(body.address).toBe(USER.address);
-        expect(body.city).toBe(USER.city);
-        expect(body.state).toBe(USER.state);
-        expect(body.country).toBe(USER.country);
-        expect(body.zipCode).toBe(USER.zipCode);
+        expect(body.cpf).toBe(user.cpf);
+        expect(body.birthday).toBe(user.birthday);
+        expect(body.name).toBe(user.name);
+        expect(body.email).toBe(user.email);
+        expect(body.address).toBe(user.address);
+        expect(body.city).toBe(user.city);
+        expect(body.state).toBe(user.state);
+        expect(body.country).toBe(user.country);
+        expect(body.zipCode).toBe(user.zipCode);
+      });
+    });
+
+    describe('WHEN creating a user with same email as another user', () => {
+      it('THEN it should return status 400 with the body containing the error message', async () => {
+        const user = createOnePOSTUser();
+        const { email } = fixedUser1;
+        const response = await request(app.getHttpServer())
+          .post('/user')
+          .send({ ...user, email });
+        const { body, status } = response;
+
+        expect(status).toBe(400);
+
+        expect(body).toEqual({
+          statusCode: expect.any(Number),
+          message: expect.any(String),
+          error: expect.any(String)
+        });
+
+        expect(body.statusCode).toBe(400);
+        expect(body.message).toBe('Duplicate cpf or email');
+        expect(body.error).toBe('Bad Request');
+      });
+    });
+
+    describe('WHEN creating a user with same cpf as another user', () => {
+      it('THEN it should return status 400 with the body containing the error message', async () => {
+        const user = createOnePOSTUser();
+        const { cpf } = fixedUser2;
+        const response = await request(app.getHttpServer())
+          .post('/user')
+          .send({ ...user, cpf });
+        const { body, status } = response;
+
+        expect(status).toBe(400);
+
+        expect(body).toEqual({
+          statusCode: expect.any(Number),
+          message: expect.any(String),
+          error: expect.any(String)
+        });
+
+        expect(body.statusCode).toBe(400);
+        expect(body.message).toBe('Duplicate cpf or email');
+        expect(body.error).toBe('Bad Request');
+      });
+    });
+
+    describe('WHEN creating a user with invalid cpf, email, password and birthday', () => {
+      it('THEN it should return status 400 with the body containing the error message', async () => {
+        const user = createOnePOSTUser();
+        const response = await request(app.getHttpServer())
+          .post('/user')
+          .send({ ...user, cpf: '12345678910', email: 'jj.com', password: '142', birthday: '12/12/2012' });
+        const { body, status } = response;
+
+        expect(status).toBe(400);
+
+        expect(body).toEqual({
+          statusCode: expect.any(Number),
+          message: expect.arrayContaining([expect.any(String)]),
+          error: expect.any(String)
+        });
+
+        expect(body.statusCode).toBe(400);
+        expect(body.message).toEqual([
+          'The CPF is invalid, it should have the format of xxx.xxx.xxx-xx.',
+          'The informed date is invalid, it should have the format of dd/mm/yyyy and be greater than 18 years from now',
+          'email must be an email',
+          'password must be longer than or equal to 6 characters'
+        ]);
+        expect(body.error).toBe('Bad Request');
       });
     });
   });
